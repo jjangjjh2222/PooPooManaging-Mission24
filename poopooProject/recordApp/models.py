ff7d78b77ff3676ee725fe64e58ca3ec56c4288c
jjangjjh2222 <jjangjjh2222@naver.com>
2022-08-28 03:30:30 +0900

    models.py 수정

--- poopooProject/recordApp/models.py
+++ poopooProject/recordApp/models.py
@@ -3,7 +3,9 @@ from django.conf import settings
 
 class Post(models.Model):
     date = models.DateTimeField(auto_now_add = True)
-    time = models.IntegerField()
+    hour = models.CharField()
+    min = models.CharField()
+    sec = models.CharField()
 
     def __str__(self):
         return self.date
--- poopooProject/recordApp/views.py
+++ poopooProject/recordApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-#from recordApp.forms import PostForm
 from recordApp.models import Post
+
 
 def record(request):
     posts = Post.objects.filter().order_by('-date')
@@ -9,16 +9,6 @@ def record(request):
 # html 파일 경로 수정
 
 
-def postcreate(request):
-    if request.method == 'POST' or request.method == "FILES":# request method가 POST일 경우
-        form = PostForm(request.POST, request.FILES)# 입력값 저장
-        if form.is_valid():
-            form.save()
-            return redirect('home') 
-
-    else: # request method가 GET일 경우
-        form = PostForm()# form 입력 html 띄우기
-    return render(request, 'post_form.html', {'form':form})
 
 def get_time(request):
 
@@ -26,5 +16,6 @@ def get_time(request):
     min_data = request.GET.get('min')
     sec_data = request.GET.get('sec')
     
+    Post.hour 
     print(hour_data, min_data, sec_data)
 
